df9b992c2c3ba2d736539159cb1419b76ebe4024
Iago <7103539+Zengor@users.noreply.github.com>
2018-11-27 19:49:16 -0300

    Add evaporation for MMAS

--- src/aco/mmas.rs
+++ src/aco/mmas.rs
@@ -13,13 +13,29 @@ pub fn construct(colony: &mut Colony) -> Vec<AntResult> {
 
 
 pub fn update_pheromones(colony: &mut Colony, results: &Vec<AntResult>) {
-    //evaporate
-    //then
+    evaporate(colony);
     
     for a in results { 
         for (&i,&j) in a.tour.iter().tuple_windows() {
             colony.pheromones[i][j] += 2.0; //TODO
             colony.pheromones[j][i] += 2.0;
+        }
+    }
+}
+
+
+
+fn evaporate(colony: &mut Colony) {
+    let evap_rate = colony.parameters.evaporation_rate;
+    let trail_min = colony.parameters.trail_min;
+    for i in 0..colony.data.size {
+        for j in 0..i {
+            let mut new_pheromone = (1.0 - evap_rate) * colony.pheromones[i][j];
+            if new_pheromone < trail_min {
+                new_pheromone = trail_min;
+            }
+            colony.pheromones[i][j] = new_pheromone;
+            colony.pheromones[j][i] = new_pheromone;
         }
     }
 }
