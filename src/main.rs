5ed0ed5ed75d3277c7f1ce4f0a62e57b452a3d97
Unknown <zengor@hotmail.com>
2018-11-30 00:04:01 -0300

    Add very basic result file output

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,34 @@ mod util;
 mod instance_data;
 mod tsplibreader;
 
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{BufWriter,Result};
 use crate::aco::{AcoParameters, run_aco};
 use crate::tsplibreader::{read_instance_file};
+use crate::aco::ResultLog;
+
+fn print_log(results: ResultLog, out_name: &str) -> Result<()> {
+    let f = File::open(out_name)?;
+    let mut writer = BufWriter::new(f);
+    let best = results.best_timestamped();
+    write!(writer, "BEST FOUND: {}", best.result.length)?;
+    write!(writer, "BEST TOUR: {:?}", best.result.tour)?;
+    write!(writer, "Found during iteration {} at {:?}", best.iteration, best.timestamp)?;
+    write!(writer, "==========================")?;
+    for (i,t) in results.log.iter().enumerate() {
+        write!(writer, "-----Iter {}, new_best: {}", i, t.is_new_best)?;
+        write!(writer, "length: {}", t.result.length)?;
+        write!(writer, "tour: {:?}", t.result.tour)?;
+    }
+    Ok(())
+}
 
 fn main() {
-    let instance_file = read_instance_file("test.tsp");
+    let start_time = std::time::Instant::now();
+    let input_file = "test.tsp";
+    let instance_file = read_instance_file(input_file);
     let parameters = AcoParameters::default();
     let results = run_aco(&instance_file.data, &parameters);
-    //do something with results
+    print_log(results, &format!("{} - {:?}", input_file, start_time));
 }
