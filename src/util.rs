fd5bd8441806cb02c4ffaf6eeebdbb6f3ff65871
Iago <7103539+Zengor@users.noreply.github.com>
2019-01-10 10:14:30 -0300

    Add function to calculate value of a tour to `util`

--- src/util.rs
+++ src/util.rs
@@ -17,6 +17,18 @@ pub fn generate_filled_matrix<T: Copy>(size: usize, element: T) -> Vec<Vec<T>> {
     out    
 }
 
+/// Calculates the value of a single tour (assumes first node is 0), used while testing, left in just in case.
+#[allow(dead_code)]
+pub fn value_of_tour(distances: &IntegerMatrix, tour: &[usize]) -> u32 {
+    use itertools::Itertools;
+    let mut length = 0;
+    for (&i, &j) in tour.iter().tuple_windows() {
+        length += distances[i][j];
+    }
+    length+= distances[tour[tour.len()-1]][tour[0]];
+    length
+}
+
 pub mod distance_funcs {
     pub fn euc_2d(i: (i32, i32), j: (i32, i32)) -> u32 {
         (((i.0 - j.0).pow(2) + (i.1 - j.1).pow(2)) as f64).sqrt().round() as u32
